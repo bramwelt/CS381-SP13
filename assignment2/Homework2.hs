5cd0cf8e082544cac334afe4b0a9a7c178a8a52c
Trevor Bramwell <trevor@bramwell.net>
2013-04-26 23:49:03 -0700

    HW2 - Working part 1, but error checking or handling

--- assignment2/Homework2.hs
+++ assignment2/Homework2.hs
@@ -8,6 +8,7 @@ data Cmd = LD Int
          | ADD
          | MULT
          | DUP
+         deriving Show
 
 type Stack = [Int]
 -- Do we instantiate a stack and work on it? should D be a Maybe Stack?? We'll
@@ -16,14 +17,30 @@ type Stack = [Int]
 
 
 -- TODO
-type D = Maybe Int 
+type D = Stack -> Stack
 -- sem :: Prog -> D
 -- semCmd :: Cmd -> D
 
+--semCmd :: Cmd -> Stack -> Stack
+semCmd :: Cmd -> D
+semCmd (LD a) = \xs         -> [a] ++ xs
+semCmd (ADD)  = \(x1:x2:xs) -> [x1+x2] ++ xs
+semCmd (MULT) = \(x1:x2:xs) -> [x1*x2] ++ xs
+semCmd (DUP)  = \(x1:xs)    -> [x1,x1] ++ xs
+
+--sem :: Prog -> Stack -> Stack
+sem :: Prog -> D
+sem [] a = a
+sem (x:xs) a = sem xs (semCmd x a)
+
+eval :: Prog -> Stack
+eval p = sem p []
+
 --Test data
-test1 = [LD 3, DUP, ADD, DUP, MULT]
-test2 = [LD 3, ADD]
-test3 = []
+test1 = [LD 3, DUP, ADD, DUP, MULT] -- [3] -> [3,3] -> [6] -> [6,6] -> [36]
+test2 = [LD 3, ADD] -- Error
+test3 = [] -- []
+test4 = [LD 2, DUP, MULT] -- [2] -> [2, 2] -> [4]
 
 {----------------------- Exercise 2 -------------------------}
 
