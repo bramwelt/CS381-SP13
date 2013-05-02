6c9890d0d9d4420880a8d99eb4b6888f58303313
Trevor Bramwell <trevor@bramwell.net>
2013-05-01 23:08:29 -0700

    HW3 - Boilerplate

---
+++ assignment3/Homework3.hs
@@ -0,0 +1,18 @@
+{- Authors:
+     Trevor Bramwell
+     Emily  Dunham
+     Daniel Reichert
+-}
+
+module Homework3 where
+
+{----------------------- Exercise 1 -------------------------}
+
+
+
+{----------------------- Exercise 2 -------------------------}
+
+
+
+{----------------------- Exercise 3 -------------------------}
+
