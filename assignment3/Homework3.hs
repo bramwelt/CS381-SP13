4269ccb1b42bd3ca719406b37d27109f84e62cb2
Trevor Bramwell <trevor@bramwell.net>
2013-05-05 21:45:08 -0700

    HW3 - Finished up ranks.
    Copied in sem related functions from HW2. Modifed sem to be easier to
    read and better defined.
    
    rankP (like eval) merely setup up the base state of a rank = 0.
    Originally I thought the beginning rank would be the rank of the first
    CMD, but that would be a circular definition.
    
    CMD now derives show, and a few test programs were added.

--- assignment3/Homework3.hs
+++ assignment3/Homework3.hs
@@ -20,6 +20,60 @@ data Cmd = LD Int
          | SWAP
          -- Pops k elements off the top of the stack.
          | POP Int
+         deriving Show
+
+type Stack = [Int]
+type D = Maybe Stack -> Maybe Stack
+
+type Rank = Int
+type CmdRank = (Int, Int)
+
+
+-- Semantics of individual Commands
+semCmd :: Cmd -> D
+semCmd (LD a) (Just xs)         = Just ([a] ++ xs)
+semCmd (ADD)  (Just (x1:x2:xs)) = Just ([x1+x2] ++ xs)
+semCmd (MULT) (Just (x1:x2:xs)) = Just ([x1*x2] ++ xs)
+semCmd (DUP)  (Just (x1:xs))    = Just ([x1,x1] ++ xs)
+semCmd _      _                 = Nothing
+
+-- Semantics of a Program
+sem :: Prog -> D
+sem [] a = a
+sem (x:xs) a = sem xs (semCmd x a)
+
+-- Evaluation 'running' a program. Starts with an empty stack.
+--
+eval :: Prog -> Maybe Stack
+eval p = sem p (Just [])
+
+-- Map each stack to its rank
+-- (n, m) n the number of elements removed from the stack
+--        m the number of elements added to the stack
+--
+rankC :: Cmd -> CmdRank
+rankC (LD _)  = (0, 1)
+rankC ADD     = (2, 1)
+rankC MULT    = (2, 1)
+rankC DUP     = (1, 2)
+rankC INC     = (1, 1)
+rankC SWAP    = (2, 2)
+rankC (POP a) = (a, 0)
+
+-- Compute the rank of a program. Nothing represents Errors.
+-- defined in terms of 'rank'
+--
+rankP :: Prog -> Maybe Rank
+rankP xs = rank xs 0
+
+-- Given a program and its rank, compute the rank outcome
+--
+rank :: Prog -> Rank -> Maybe Rank
+rank []     r | r >= 0     = Just r
+rank (x:xs) r | under >= 0 = rank xs (under+adds)
+              where (subs, adds) = rankC x
+                    under        = r - subs
+rank _      _ = Nothing
 
 p1 = [LD 3, DUP, ADD, LD 5, SWAP] -- Just [6, 5]
 p2 = [LD 8, POP 1, LD 3, DUP, POP 2, LD 4] -- Just [4]
@@ -27,38 +81,6 @@ p3 = [LD 3, LD 4, LD 5, MULT, ADD] -- Just [23]
 p4 = [LD 2, ADD] -- Nothing
 p5 = [DUP] -- Nothing
 p6 = [POP 1] -- Nothing
-
-{-
-Rank (n, m) 
-  - n is the number of elements the operation takes from the stack.
-  - m is the number of elements the operation puts on the stack.
-
-  The rank for a program is defined as the rank that would be obtained
-  if the program were run on an empty stack.
-
-  A rank error indicates stack underflow
--}
-
-type Rank = Int
-type CmdRank = (Int, Int)
-
--- Map each stack to its rank
-rankC :: Cmd -> CmdRank
-rankC (LD a)  = (0, 1)
-rankC ADD     = (2, 1)
-rankC MULT    = (2, 1)
-rankC DUP     = (1, 1)
-rankC INC     = (1, 1)
-rankC SWAP    = (2, 2)
-rankC (POP i) = (1, 0)
-
--- Compute the rank of a program. Nothing represents Errors.
--- defined in terms of 'rank'
-{- TODO rankP :: Prog -> Maybe Rank -}
-
-
--- Given a program and its rank, compute the rank outcome
-{- TODO rank :: Prog -> Rank -> Maybe Rank -}
 
 
 {--- Part (b) ---}
