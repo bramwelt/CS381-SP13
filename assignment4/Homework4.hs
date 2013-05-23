2b946807d0eeaf433a0366bfe0fe79ac6d9086e7
Trevor Bramwell <bramwelt@osuosl.org>
2013-05-23 09:39:22 -0700

    HW4 - Finished part2

--- assignment4/Homework4.hs
+++ assignment4/Homework4.hs
@@ -39,24 +39,40 @@
 
 {- Exercise 2: Static and Dynamic Scope
 
-   []
+   Note: <x,y> denotes <static, dynamic>
+
+   [ ]
 1  [x:?]
 2  [y:?, x:?]
 3  [z:?, y:?, x:?]
 4  [z:?, y:?, x:3]
 5  [z:?, y:7, x:3]
 6  [f:{}, z:?, y:7, x:3]
-15 [z:?, y:7, x:3]
+7  [y:?, f:{}, z:?, y:7, x:3]
+8  [y:11, f:{}, z:?, y:7, x:3]
+9  [g:{}, y:11, f:{}, z:?, y:7, x:3]
+10 [y:?, g:{}, y:11, f:{}, z:?, y:7, x:3]
+11 [y:13, g:{}, y:11, f:{}, z:?, y:7, x:3]
+12 >>
+   9 [x:2, y:13, g:{}, y:11, f:{}, z:?, y:7, x:3]
+     >>
+     6 [res: <21, 26>, y: <7, 13>, x:2, y:13, g:{}, y:11, f:{}, z:?, y:7, x:3]
+     <<
+   9 [res: <21, 26>, x:2, y:13, g:{}, y:11, f:{}, z:?, y:7, x:3]
+   <<
+12 [y:13, g:{}, y:11, f:{}, z:<21, 26>, y:7, x:3]
+13 [g:{}, y:11, f:{}, z:<21, 26>, y:7, x:3]
+14 [y:11, f:{}, z:<21, 26>, y:7, x:3]
+15 [z:<21, 26>, y:7, x:3]
 16 [ ]
 
 (a) Which value will be assigned to z in line 12 under static scoping?
 
-    Line 12 is never called.
-
+    z: 21
 
 (b) Which value will be assigned to z in line 12 under dynamic scoping?
 
-    Line 12 is never called.
+    z: 26
 
 -}
 
